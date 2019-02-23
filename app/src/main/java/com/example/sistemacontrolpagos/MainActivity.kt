fa2d3fd44689e70a01820a630fca9340c1e925dd
elgael06 <elgael06@gmail.com>
2019-02-23 15:02:41 -0700

    Recycle View

--- app/src/main/java/com/example/sistemacontrolpagos/MainActivity.kt
+++ app/src/main/java/com/example/sistemacontrolpagos/MainActivity.kt
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.Layout
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.*
+import com.example.sistemacontrolpagos.Manejadores.Clientes_adapter
 import com.example.sistemacontrolpagos.Manejadores.Usuario_manager
 import com.example.sistemacontrolpagos.Manejadores.myAdapter
 import com.example.sistemacontrolpagos.Modelos.Usuario
@@ -15,6 +18,8 @@ import com.example.sistemacontrolpagos.Vistas.EdicionCliente
 import com.example.sistemacontrolpagos.Vistas.Moviminetos_cuentas
 import com.example.sistemacontrolpagos.Vistas.Saldo_cliente
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_cliente.view.*
+
 //import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
@@ -26,48 +31,31 @@ class MainActivity : AppCompatActivity() {
         //val recicle:RecyclerView = find(R.id.Contenedor_clientes)
 
         llenarUsuarios()
-
-        Ir_edicion.setOnClickListener{
-            Ir_a_edicion()
-        }
-        Ir_moviminetos.setOnClickListener{
-            Ir_a_moviminetos()
-        }
-        Ir_saldos.setOnClickListener{
-            Ir_a_saldos()
-        }
         actualizar.setOnClickListener{
             llenarUsuarios()
         }
-      }
-
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menus_acciones_clientes,menu)
         return true
     }
-
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         var id:Int = item?.itemId ?: 0
         when(id){
-            R.id.menu_nuevo->{
-                Ir_a_edicion()
+            R.id.menu_nuevo->{Ir_a_edicion(0)
             }
-            R.id.menu_editar->{
-                Ir_a_saldos()
+            R.id.menu_editar->{Ir_a_edicion(1)
             }
-            R.id.menu_saldos->{
-                Ir_a_saldos()
+            R.id.menu_saldos->{Ir_a_saldos()
             }
-            R.id.menu_moviminetos->{
-                Ir_a_moviminetos()
+            R.id.menu_moviminetos->{Ir_a_moviminetos()
             }
-            R.id.menu_cerrar->{
-                finish()
+            R.id.menu_cerrar->{finish()
             }
         }
         return super.onOptionsItemSelected(item)
     }
-    fun Ir_a_edicion(){
+    fun Ir_a_edicion(usuario:Int?){
         Toast.makeText(applicationContext,"Edicion", Toast.LENGTH_SHORT).show()
         var vista: Intent = Intent(this, EdicionCliente() ::class.java)
         startActivity(vista)
@@ -85,25 +73,26 @@ class MainActivity : AppCompatActivity() {
 
     fun llenarUsuarios( ){
         val lista_ = Usuario_manager().Todos(this)
-        val lista:List<String> = listOf("uno","dos","tres")
-        //recicle.layoutManager = LinearLayoutManager(this)
-        //recicle.adapter = myAdapter(lista)
 
+        tabla_usuarios.removeAllViewsInLayout()
         tabla_usuarios.clearDisappearingChildren()
         lista_?.forEach {agregar_usuario_a_lista(it)}
+
+        Lista_clientes.layoutManager = LinearLayoutManager(this)
+        Lista_clientes.hasFixedSize()
+        Lista_clientes.adapter = Clientes_adapter(lista_)
+
+        Lista_clientes.layoutManager = LinearLayoutManager(this)
+        Lista_clientes.adapter = Clientes_adapter(lista_)
 
    //
     }
     fun agregar_usuario_a_lista(valor:Usuario){
         var fila = TableRow(applicationContext)
-        var id = TextView(applicationContext)
         var nombre = TextView(applicationContext)
-        println("nombre=${nombre}")
 
-        id.text = valor.codigo.toString()
         nombre.text = valor.nombre
 
-        fila.addView(id)
         fila.addView(nombre)
 
         tabla_usuarios.addView(fila, TableLayout.LayoutParams(
